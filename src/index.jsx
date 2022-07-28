865071d294df8e54e2191ba36d4ae8110234a49d
mswider <matthew.swider@outlook.com>
2022-07-27 19:40:57 -0500

    Log version in frontend

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,10 @@ import { HashRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 
+const message = `GoGuardian Monitor v${APP_VERSION}
+Source code: https://github.com/mswider/Monitor`;
+console.log(message);
+
 ReactDOM.render(
   <HashRouter>
     <App />
