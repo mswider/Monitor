1e5137b76839cb74aec93b4184cfbe30fd7118e3
mswider <matthew.swider@outlook.com>
2023-08-24 11:23:45 -0500

    Changes

--- src/Session/Main.jsx
+++ src/Session/Main.jsx
@@ -111,8 +111,6 @@ function Main({ device, sessions, session, error, magicNumber }) {
   useEffect(() => {
     if (device.isVerified) {
       setControlName(`private-subaccount.${device.sid}`);
-    } else {
-      console.log('Skipping connection to control channel: device is not verified');
     }
   }, []);
 
